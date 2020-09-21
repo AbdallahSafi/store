8a77139ce492c2064acc74e86d604315ed565e18
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-09-21 16:17:27 +0300

    set the auth url

--- src/github.js
+++ src/github.js
@@ -9,7 +9,7 @@ const users = require('./users.js');
 */
 
 const tokenServerUrl = process.env.TOKEN_SERVER;
-const remoteAPI = process.env.REMOTE_API;
+// const remoteAPI = process.env.REMOTE_API;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const API_SERVER = process.env.API_SERVER;
