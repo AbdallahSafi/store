e12c2cb91a71a55b882aa3b6529662fe75db156a
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-09-21 23:48:14 +0300

    update

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.static('./public'));
 
 // Routes
 app.get('/oauth', oauth, (req, res) => {
-  res.status(200).json({token: req.token, user: req.user});
+  res.status(200).json({token: req.token});
 });
 
 module.exports = {
